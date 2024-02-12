132d83d22846d73a1d3930be83bb22c9695fe455
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-02-12 20:39:03 +0200

    [88] [Time Beats: 90.77%] [Memory Beats: 37.87%] - LeetPush

---
+++ 88-Merge-Sorted-Array.py
@@ -0,0 +1,9 @@
+class Solution:
+  def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    while n > 0:
+        if nums1[m-1] >= nums2[n-1] and m>0:
+            nums1[m+n-1] = nums1[m-1]
+            m -= 1
+        else:
+            nums1[m+n-1] = nums2[n-1]
+            n -= 1
