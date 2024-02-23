88030c1a0ba0bd8368d490e17ed20b6fb4acb8a3
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-02-23 17:36:18 +0200

    [7] [Time Beats: 29.78%] [Memory Beats: 100.00%] - LeetPush

---
+++ 7-Reverse-Integer.cpp
@@ -0,0 +1,12 @@
+class Solution {                      
+public:
+    int reverse(int x) {
+        int r=0;      
+        while(x){
+         if (r>INT_MAX/10 || r<INT_MIN/10) return 0; 
+         r=r*10+x%10; 
+         x=x/10;     
+        } 
+        return r;  
+    }
+}; 
