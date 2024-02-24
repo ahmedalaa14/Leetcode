af223917a12f1afc83bd9dc7b9269b5d0c3f05d2
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-02-24 17:21:28 +0200

    [1768] [Time Beats: 74.11%] [Memory Beats: 63.00%] - LeetPush

---
+++ 1768-Merge-Strings-Alternately.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
+        ans = []
+        while i < len(word1) and j < len(word2):
+            ans.append(word1[i])
+            ans.append(word2[j])
+            i += 1
+            j += 1
+        while i < len(word1):
+            ans.append(word1[i])
+            i += 1
+        while j < len(word2):
+            ans.append(word2[j])
+            j += 1
+        return "".join(ans)
