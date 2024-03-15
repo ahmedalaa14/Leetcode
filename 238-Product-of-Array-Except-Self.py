978973e483d33dfda1e17d56aaaac7d4c6c3dd51
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-03-15 02:21:58 +0200

    [238] [Time Beats: 20.49%] [Memory Beats: 33.42%] - LeetPush

--- 238-Product-of-Array-Except-Self.py
+++ 238-Product-of-Array-Except-Self.py
@@ -1,13 +1,16 @@
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+class Solution(object):
+    def productExceptSelf(self, nums):
         n = len(nums)
-        prefix_product = 1
-        postfix_product = 1
-        result = [0]*n
-        for i in range(n):
-            result[i] = prefix_product
-            prefix_product *= nums[i]
-        for i in range(n-1,-1,-1):
-            result[i] *= postfix_product
-            postfix_product *= nums[i]
-        return result
+        
+        prefix = [1] * n
+        suffix = [1] * n
+        
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+        
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+        
+        answer = [prefix[i] * suffix[i] for i in range(n)]
+        
+        return answer
