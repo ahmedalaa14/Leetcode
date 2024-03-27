a41605985f3b60c657bbf03ba410d0b8d1f5c2a5
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-03-27 20:45:40 +0200

    [643] [Time Beats: 78.91%] [Memory Beats: 31.27%] - LeetPush

---
+++ 643-Maximum-Average-Subarray-I.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double ans  = 0;
+        double sum = 0;
+        for(int i =0; i < k;i++)
+        {
+            sum += nums[i];
+        }
+        ans = sum; 
+        for(int i = k ;i < nums.size();i++)
+        {
+            sum += nums[i];
+            sum -= nums[i-k];
+            ans = max(ans,sum);
+        }
+        return ans/k ;
+    }
+};
