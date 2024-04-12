1236e03811757b8355b358d1a62b6434c2834b5a
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-04-12 20:00:02 +0200

    [42] [Time Beats: 80.88%] [Memory Beats: 87.28%] - LeetPush

--- 42-Trapping-Rain-Water.py
+++ 42-Trapping-Rain-Water.py
@@ -1,26 +1,17 @@
 class Solution(object):
     def trap(self, height):
-        n = len(height)
-        if n == 0:
-            return 0
-        
-        left = [0] * n
-        right = [0] * n
-        
-        # Fill left array
-        left[0] = height[0]
-        for i in range(1, n):
-            left[i] = max(left[i - 1], height[i])
-        
-        # Fill right array
-        right[n - 1] = height[n - 1]
-        for i in range(n - 2, -1, -1):
-            right[i] = max(right[i + 1], height[i])
-        
-        # Calculate trapped water
-        trappedWater = 0
-        for i in range(n):
-            trappedWater += min(left[i], right[i]) - height[i]
-        
-        return trappedWater     
-        
+        i = 0
+        left_max = height[0]
+        sum = 0
+        j = len(height) - 1
+        right_max = height[j]
+        while i < j:
+            if left_max <= right_max:
+                sum += left_max - height[i]
+                i += 1
+                left_max = max(left_max, height[i])
+            else:
+                sum += right_max - height[j]
+                j -= 1
+                right_max = max(right_max, height[j])
+        return sum
