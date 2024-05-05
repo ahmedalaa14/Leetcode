cd6ce311dca3df8ef99e98f2f9fa104f0f76d7f5
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-05-05 21:36:24 +0300

    [237] [Time Beats: 21.76%] [Memory Beats: 64.85%] - LeetPush

---
+++ 237-Delete-Node-in-a-Linked-List.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def deleteNode(self, node):
+        
+        while node.next.next:
+            node.val = node.next.val
+            node = node.next
+        node.val = node.next.val
+        node.next = None
+
+        
