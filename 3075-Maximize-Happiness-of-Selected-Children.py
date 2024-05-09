94b4373f1f9804dc7f39b5692272dac8e99e65b8
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-05-09 20:56:16 +0300

    [3075] [Time Beats: 50.93%] [Memory Beats: 7.99%] - LeetPush

---
+++ 3075-Maximize-Happiness-of-Selected-Children.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maximumHappinessSum(self, happiness, k):
+        happiness.sort(reverse=True)
+        i = 0
+        res = 0
+
+        while k > 0:
+            happiness[i] = max(happiness[i] - i, 0)
+            res += happiness[i]
+            i += 1
+            k -= 1
+
+        return res
+        
