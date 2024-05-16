04a45b73317b77742b0fa92ef37f585272cb2e52
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-05-16 04:13:17 +0300

    [2331] [Time Beats: 78.79%] [Memory Beats: 34.85%] - LeetPush

---
+++ 2331-Evaluate-Boolean-Binary-Tree.go
@@ -0,0 +1,16 @@
+func evaluateTree(root *TreeNode) bool {
+    if root.Val == 1 {
+        return true
+    }
+    if root.Val == 0 {
+        return false
+    }
+    if root.Val == 2 {
+        return evaluateTree(root.Left) || evaluateTree(root.Right)
+    }
+    if root.Val == 3 {
+        return evaluateTree(root.Left) && evaluateTree(root.Right)
+    }
+
+    return true
+}
