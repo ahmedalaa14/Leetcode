fc05326d1184176ef511cfb418b25ec284d718a0
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-05-17 21:16:24 +0300

    [1325] [Time Beats: 16.00%] [Memory Beats: 20.00%] - LeetPush

---
+++ 1325-Delete-Leaves-With-a-Given-Value.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeLeafNodes(self, root, target):
+        if not root:
+            return None
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+        if not root.left and not root.right and root.val == target:
+            return None
+        return root
+        
