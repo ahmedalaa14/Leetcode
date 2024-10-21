dd9d9e6b947619ba9e6056601e4099ece912d743
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2024-10-21 22:21:53 +0300

    [1593] [Time Beats: 92.86%] [Memory Beats: 28.57%] - LeetPush

---
+++ 1593-Split-a-String-Into-the-Max-Number-of-Unique-Substrings.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxUniqueSplit(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        def dfs(start, seen):
+            if start == len(s):
+                return 0
+
+            max_splits = 0
+            for i in range(start + 1, len(s) + 1):
+                substring = s[start:i]
+
+                if substring not in seen:
+                    seen.add(substring)
+                    max_splits = max(max_splits, 1 + dfs(i, seen))
+                    seen.remove(substring)  
+
+            return max_splits
+
+        return dfs(0, set())
