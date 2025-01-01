8c8194ab8ea39f0a5f1dcc25791d9e08145404c8
Ahmed Alaa <148467567+ahmedalaa14@users.noreply.github.com>
2025-01-01 20:57:59 +0200

    [1422] [Time Beats: 57.89%] [Memory Beats: 9.48%] - LeetPush

---
+++ 1422-Maximum-Score-After-Splitting-a-String.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        total_zeros = s.count('0')
+        ans , zeros , n = -1 , 0 , len(s)
+        for i in range(1,n):
+            if s[i-1] == '0':
+                zeros +=1
+                total_zeros -=1
+            ans = max ( ans , zeros + (n - total_zeros - i)) 
+        return ans       
+        
